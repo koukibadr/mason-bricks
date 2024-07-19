ad8cceb0b7ea3776d3ea63e938d3a5b04a4b812c
koukibadr <badreddinekouki1998@gmail.com>
2024-07-19 21:17:50 +0100

    Model_generator brick: support List, Map and DateTime object

--- model_generator/hooks/pre_gen.dart
+++ model_generator/hooks/pre_gen.dart
@@ -6,7 +6,7 @@ void run(HookContext context) {
   File? file;
   try {
     file = File("${Directory.current.path}/${context.vars['class_name']}.json");
-  } catch (e) {
+  } on PathNotFoundException {
     context.logger.err(
       "{{class_name}}.json not found, make sure you have a json file with the exact dart class name under ${Directory.current.path}",
     );
@@ -16,20 +16,52 @@ void run(HookContext context) {
   var fileContent = file.readAsStringSync();
   var fileContentAsJson = jsonDecode(fileContent) as Map;
 
-  List<String> listOfAttributes = [];
-  List<String> namesOfAttributes = [];
+  List<Map<String, dynamic>> attributes = [];
   for (var element in fileContentAsJson.entries) {
-    listOfAttributes.add(
-      "${element.value.runtimeType.toString()} ${element.key}",
-    );
-    namesOfAttributes.add('${element.key}');
+    var attributeType = getAttributeType(element.value);
+    attributes.add({
+      'type': attributeType.toString().replaceAll('_', ''),
+      'name': element.key,
+      'defaultValue': getDefaultAttributeValue(attributeType),
+    });
   }
 
   context.vars = {
     ...context.vars,
     ...{
-      'listOfAttributes': listOfAttributes,
-      'namesOfAttributes': namesOfAttributes,
+      'attributes': attributes,
     }
   };
+}
+
+Type getAttributeType(dynamic attributeValue) {
+  if (attributeValue.runtimeType == String) {
+    var dateTimeValue = DateTime.tryParse(attributeValue);
+    if (dateTimeValue != null) {
+      return DateTime;
+    } else {
+      return String;
+    }
+  } else {
+    return attributeValue.runtimeType;
+  }
+}
+
+dynamic getDefaultAttributeValue(Type attributeType) {
+  switch (attributeType) {
+    case int:
+      return 0;
+    case String:
+      return "\"\"";
+    case bool:
+      return false;
+    case double:
+      return 0.0;
+    case List:
+      return [];
+    case DateTime:
+      return "DateTime.now()";
+    default:
+      return Map();
+  }
 }
